bc29304b8745cdeda0596cf3e85d5784c32692a0
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-08-20 04:46:34 +0000

    * Minimize write calls with local buffer
    * Print modified string

--- main.h
+++ main.h
@@ -2,11 +2,15 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char);
-void handle_specifier(char fmt, va_list args, int *num_of_printed_chars);
+void handle_specifier(char fmt,
+		int *num_of_printed_chars, va_list args, char buffer[]);
 int dec_to_bin(int, int *num_of_printed_chars);
+void print_mod_string(char *str, int *num_of_printed_chars, char buffer[]);
+void print_string(char *str, int *num_of_printed_chars, char buffer[]);
 
 #endif /*MAIN_H*/
 
